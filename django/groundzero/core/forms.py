c73bd22a22c86bdbf5df379f9944239b8bb37e08
BenjaPino <81177762+BenjaPino@users.noreply.github.com>
2021-06-16 16:32:02 -0400

    Delete django/groundzero/core directory

--- django/groundzero/core/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.db.models import fields
-from django.forms import ModelForm
-from .models import Artista
-
-class ArtistaForm(ModelForm):
-    class Meta:
-        model = Artista
-        field=['Nombre','Descripcion','Imagen']
